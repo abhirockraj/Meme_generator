a322c454585f8a5211abd1189fc0e278d4c365d7
abhirockraj <abhinavkool19@gmail.com>
2022-05-28 14:29:05 +0530

    project completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css'
+import Header from './Component/Header';
+import Main from './Component/Main';
+export default function App(){
+  return(
+    <div>
+        <Header />
+        <Main /> 
+  </div>
+  );
+};
